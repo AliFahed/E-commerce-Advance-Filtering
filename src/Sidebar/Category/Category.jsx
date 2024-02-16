1c46071ffd93fc9aea58e149e942aa2c65714af3
Ali Fahed <alifahed67@gmail.com>
2024-02-16 22:19:24 +0800

    Update Category

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -2,5 +2,36 @@ import React from "react";
 import "./Category.css";
 
 export default function Category() {
-  return <div>Category</div>;
+  return (
+    <section>
+      <h2 className="sidebar-title">Category</h2>
+
+      <div>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneakers
+        </label>
+
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+      </div>
+    </section>
+  );
 }
