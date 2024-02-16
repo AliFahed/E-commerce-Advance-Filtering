2cc94ec2f9d326ffd96cd6595670d620e9f07eef
Ali Fahed <alifahed67@gmail.com>
2024-02-16 22:29:50 +0800

    Update Colors

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -2,5 +2,39 @@ import React from "react";
 import "./Colors.css";
 
 export default function Colors() {
-  return <div>Colors</div>;
+  return (
+    <section>
+      <h2 className="sidebar-title-color-title">Colors</h2>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>All
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Black
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Blue
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Red
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>Green
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test3" />
+        <span className="checkmark"></span>White
+      </label>
+    </section>
+  );
 }
