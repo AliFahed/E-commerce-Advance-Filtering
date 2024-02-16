a6c9b75e7169dcc3d6a41b378cfa7c0e032aa6d9
Ali Fahed <alifahed67@gmail.com>
2024-02-16 17:58:08 +0800

    Update Navigation

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import "./Navigation.css";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 export default function Nav() {
-  return <nav>Navigation</nav>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input type="text" className="search-input" placeholder="Search" />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
