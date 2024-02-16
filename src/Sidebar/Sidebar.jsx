3dd721fa0534a8d0c8776f62049ab9f4420d88ca
Ali Fahed <alifahed67@gmail.com>
2024-02-16 21:59:41 +0800

    Update Sidebar

---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Sidebar.css";
+import { FaShoppingCart } from "react-icons/fa";
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+import Colors from "./Colors/Colors";
+
+export default function Sidebar() {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+          <FaShoppingCart className="sidebar-icons" />
+        </div>
+
+        <Category />
+        <Price />
+        <Colors />
+      </section>
+    </>
+  );
+}
