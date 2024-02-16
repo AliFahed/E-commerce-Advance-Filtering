c3bbf77a0fedfd7e8b7ea8622908a1e87cbe5931
Ali Fahed <alifahed67@gmail.com>
2024-02-16 22:47:58 +0800

    Refactor Static Products

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
 
 export default function Card() {
-  return <div></div>;
+  return (
+    <div className="card">
+      <img
+        src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+        alt="Shoe"
+      />
+
+      <div className="card-details">
+        <h3 className="card-title">Shoe</h3>
+        <div className="card-reviews">
+          <FaStar className="rating-star" />
+          <FaStar className="rating-star" />
+          <FaStar className="rating-star" />
+          <FaStar className="rating-star" />
+          <span className="total-reviews"></span>
+        </div>
+      </div>
+
+      <div className="card-price">
+        <p className="price">$300</p>
+        <div className="bag">
+          <FaBagShopping className="bag-icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,35 +1,13 @@
 import React from "react";
 import "./Products.css";
-import { FaStar } from "react-icons/fa";
-import { FaBagShopping } from "react-icons/fa6";
+import Card from "../Components/Card";
 
 export default function Products() {
   return (
     <section className="card-container">
-      <div className="card">
-        <img
-          src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
-          alt="Shoe"
-        />
-
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <div className="card-reviews">
-            <FaStar className="rating-star" />
-            <FaStar className="rating-star" />
-            <FaStar className="rating-star" />
-            <FaStar className="rating-star" />
-            <span className="total-reviews"></span>
-          </div>
-        </div>
-
-        <div className="card-price">
-          <p className="price">$300</p>
-          <div className="bag">
-            <FaBagShopping className="bag-icon" />
-          </div>
-        </div>
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </section>
   );
 }
