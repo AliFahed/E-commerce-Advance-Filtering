c90c2dbf4385462aefa80082e9313e8fc338dd29
Ali Fahed <alifahed67@gmail.com>
2024-02-16 22:25:35 +0800

    Update Price

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -2,5 +2,34 @@ import React from "react";
 import "./Price.css";
 
 export default function Price() {
-  return <div>Price</div>;
+  return (
+    <section className="ml">
+      <h2 className="sidebar-title-price-title">Price</h2>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>All
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>$0 -50
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>$50 -100
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>$100 - 150
+      </label>
+
+      <label htmlFor="" className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Over $150
+      </label>
+    </section>
+  );
 }
